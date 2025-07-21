d18355be709ec273785d19f111e8bc7af9710287
kwaw-fredrick <kwawfredrick35@gmail.com>
2025-07-21 13:16:22 -0900

    notifs and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,11 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express";
-import userRouthes from "./routes/user.route.js"
-import postRouthes from "./routes/post.route.js"
-import commentRouthes from "./routes/comment.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js"
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
 
 
 
@@ -13,13 +15,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => res.send("Hello from the server"));
 
-app.use("/api/users", userRouthes)
-app.use("/api/posts", postRouthes)
-app.use("/api/comment", commentRouthes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/notifications", notificationRoutes)
 
 // error handling middleware
 app.use((err, req, res, next) => {
