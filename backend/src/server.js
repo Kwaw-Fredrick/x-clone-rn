4f7aa8b2bd568d13f5ba080918b94180343456cc
kwaw-fredrick <kwawfredrick35@gmail.com>
2025-07-21 22:39:31 -0900

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,13 +35,17 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
   try {
     await connectDB();
-
-    app.listen(ENV.PORT, () =>
-      console.log("Server is up and running on PORT:", ENV.PORT)
-    );
+   // listen for local development
+   if(ENV.NODE_ENV !== "production"){ 
+    app.listen(ENV.PORT, () =>console.log("Server is up and running on PORT:", ENV.PORT)
+   )};
+   
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
   }
 };
-(startServer());
+startServer();
+
+//export for vercel
+export default app;
